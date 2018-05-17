072575b317bb0ed18c506a40ac0d5fbbe185221f
medelhanine <mohamedelhanine94@gmail.com>
2018-05-17 16:10:45 +0000

    first commit koke app

---
+++ src/main/java/guru/spring/joke/joke_app/JokeAppApplication.java
@@ -0,0 +1,12 @@
+package guru.spring.joke.joke_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JokeAppApplication.class, args);
+	}
+}
